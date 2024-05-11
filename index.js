b43d0aae5b86b5cfbbb9e68c6a36115bbc8bc838
Jinu Vijayan <jinuvijayan01@gmail.com>
2024-05-11 10:23:15 +0530

    Add: scrapped data from site and placed it in an array

---
+++ index.js
@@ -0,0 +1,36 @@
+const cheerio = require("cheerio");
+const axios = require("axios");
+const fs = require("fs");
+
+const productDetailsArray = [];
+
+async function getDataFromSite(){
+    const htmlData = await axios({
+        method : "get",
+        url : "https://www.fnp.com/all-cakes-lp?promo=redirectionsearch",
+        headers: {
+            "content-type" : "text/html"
+        }
+    })
+
+    fs.writeFileSync("./webScrappedData.txt",htmlData.data)
+}
+
+// getDataFromSite();
+
+const htmlDataFromSite = fs.readFileSync("webScrappedData.txt",{encoding: "utf-8"});
+
+const $ = cheerio.load(htmlDataFromSite);
+
+const products = $(".jss18").find(".products").each((index,elem)=>{
+    const productName = $(elem).find(".product-card_product-title__32LFp").text();
+    const productPrice = $(elem).find(".product-card_product-desc-tile__10UVW").find(".product-card_product-price-info-container__E9rQf").find(".product-card_product-price-info__17tj7").text();
+
+    const productDetails = {
+        productName : productName,
+        productPrice : productPrice
+    }
+
+    productDetailsArray.push(productDetails);
+});
+console.log(productDetailsArray)
